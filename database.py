ba3e376fe1c7d7d87a888c6650422411d1798952
Manuel Rech <63170478+manuelrech@users.noreply.github.com>
2024-09-26 23:49:08 +0200

    update: path for deployed db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://manuel:1234@localhost:5432/membox"
+DATABASE_URL = "postgresql://manuel:jkiILkYqzCm9d8SJtLprJn85M7wRr6SX@dpg-crqtceqj1k6c73dm07vg-a.frankfurt-postgres.render.com/membox"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
