33d5c65ac90d92caee7934b97ec15e9e31856a5a
Manuel Rech <63170478+manuelrech@users.noreply.github.com>
2024-09-26 23:51:13 +0200

    db url in env var

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-DATABASE_URL = "postgresql://manuel:jkiILkYqzCm9d8SJtLprJn85M7wRr6SX@dpg-crqtceqj1k6c73dm07vg-a.frankfurt-postgres.render.com/membox"
-
+DATABASE_URL = os.getenv("DATABASE_URL")#DATABASE_URL = "postgresql://manuel:jkiILkYqzCm9d8SJtLprJn85M7wRr6SX@dpg-crqtceqj1k6c73dm07vg-a.frankfurt-postgres.render.com/membox"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
